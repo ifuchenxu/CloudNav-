7d3fe42c3133d8ca33326d6c07f320895bf83a83
ifuchenxu <ifuchenxu@gmail.com>
2026-06-08 17:25:04 +0800

    Update SettingsModal.tsx

--- components/SettingsModal.tsx
+++ components/SettingsModal.tsx
@@ -44,7 +44,7 @@ const generateSvgIcon = (text: string, color1: string, color2: string) => {
 
     try {
         const encoded = window.btoa(unescape(encodeURIComponent(svg)));
-        return `data:image/svg+xml;base64,${encoded}`;
+        return "./favicon.png";
     } catch (e) {
         console.error("SVG Icon Generation Failed", e);
         return '';
